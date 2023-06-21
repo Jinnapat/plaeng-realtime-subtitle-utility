8be21f86a9cfb3a4b6de35da0e106f2cf08530df
Jinnapat Yana <jaja_yana@hotmail.com>
2023-06-21 15:02:39 +0700

    Adapt participant send speech from speaker module

--- frontend/components/participant.tsx
+++ frontend/components/participant.tsx
@@ -1,15 +1,33 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { colorTheme, speechToTextParameter } from "@/uitls/constants";
 import {
   defaultTranslateLanguage,
   languageSpeechTags,
   languageTranslateTag,
+  speechToTranslate,
 } from "@/uitls/language";
-import { RESTConstant } from "@/uitls/restUtil";
 import { SocketConstant } from "@/uitls/socketUtil";
-import { Stack, Heading, Select, Button, Box, Text } from "@chakra-ui/react";
-import axios, { AxiosError } from "axios";
+import {
+  Stack,
+  Heading,
+  Button,
+  Box,
+  Select,
+  Text,
+  useDisclosure,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import { io } from "socket.io-client";
 
 const socket = io(SocketConstant.baseUrl || "ws://localhost:8080", {
@@ -26,21 +44,41 @@ interface subtitleDto {
 }
 
 export function Participant() {
+  const {
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable,
+    transcript,
+    resetTranscript,
+  } = useSpeechRecognition();
+  const [modalText, setModalText] = useState("");
   const [language, setLanaguage] = useState(languageSpeechTags[0].tag);
+  const [subtitleLanguage, setSubtitleLanguage] = useState(
+    languageTranslateTag[0].tag
+  );
+  const [listening, setListenning] = useState(false);
   const [sessionId, setSessionId] = useState("");
-  const [errorMessage, setErrorMessage] = useState<string | undefined>("");
   const [currentSubtitle, setCurrentSubtitle] = useState("");
   const [subtitleHistory, setSubtitleHistory] = useState<string[]>([]);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const router = useRouter();
 
+  const lastEmissionRef = useRef<any>();
+  const languageRef = useRef<any>();
   const transcriptContainer = useRef<any>();
+  const transcriptRef = useRef<any>();
   const subtitleHistoryRef = useRef<any>();
   const currentSubtitleRef = useRef<any>();
   const expectedSeqRef = useRef<number>(-1);
   const bufferRef = useRef<Map<number, subtitleDto>>(
     new Map<number, subtitleDto>()
   );
+  const sequenceRef = useRef<any>(0);
 
+  languageRef.current = language;
+  lastEmissionRef.current = "";
+  transcriptRef.current = transcript;
   subtitleHistoryRef.current = subtitleHistory;
   currentSubtitleRef.current = currentSubtitle;
 
@@ -64,101 +102,194 @@ export function Participant() {
   }
 
   useEffect(() => {
+    console.log("run");
     if (!router.isReady) return;
     const query = new URLSearchParams(window.location.search);
     const session = query.get("sessionId");
-    socket.on("connect", () => {
-      socket.emit(
-        "joinSession",
-        {
-          language: defaultTranslateLanguage,
-          sessionId: session,
-        },
-        (res: any) => {
-          if (res == true && session !== null) {
-            setErrorMessage("");
-            setSessionId(session);
+    if (sessionId == "") {
+      socket.on("connect", () => {
+        socket.emit(
+          "joinSession",
+          {
+            language: defaultTranslateLanguage,
+            sessionId: session,
+          },
+          (res: any) => {
+            if (res == true && session !== null) {
+              setSessionId(session);
+            }
           }
-        }
-      );
-    });
-    socket.on("exception", (e) => {
-      setErrorMessage(e.status + " : " + e.message);
-    });
-    socket.on("subtitle", (e) => {
-      transcriptContainer.current?.scrollIntoView({ behavior: "smooth" });
-      if (expectedSeqRef.current == -1) {
-        expectedSeqRef.current = e.seq + 1;
-        if (e.isBreak) {
-          setSubtitleHistory((old) => [...old, currentSubtitleRef.current]);
-          setCurrentSubtitle("");
-        } else {
-          setCurrentSubtitle(e.speech);
-        }
-      } else {
-        if (expectedSeqRef.current == e.seq) {
+        );
+      });
+      socket.on("subtitle", (e) => {
+        transcriptContainer.current?.scrollIntoView({ behavior: "smooth" });
+        if (expectedSeqRef.current == -1) {
+          expectedSeqRef.current = e.seq + 1;
           if (e.isBreak) {
             setSubtitleHistory((old) => [...old, currentSubtitleRef.current]);
             setCurrentSubtitle("");
           } else {
             setCurrentSubtitle(e.speech);
           }
-          fillGap();
-        } else if (e.seq > expectedSeqRef.current) {
-          bufferRef.current.set(e.seq, {
-            speech: e.speech,
-            isBreak: e.isBreak,
-          });
-          fillGap();
+        } else {
+          if (expectedSeqRef.current == e.seq) {
+            if (e.isBreak) {
+              setSubtitleHistory((old) => [...old, currentSubtitleRef.current]);
+              setCurrentSubtitle("");
+            } else {
+              setCurrentSubtitle(e.speech);
+            }
+            fillGap();
+          } else if (e.seq > expectedSeqRef.current) {
+            bufferRef.current.set(e.seq, {
+              speech: e.speech,
+              isBreak: e.isBreak,
+            });
+            fillGap();
+          }
         }
-      }
-    });
-    socket.on("sessionEnd", (e) => {
-      setErrorMessage("This session has ended");
-    });
-    socket.connect();
+      });
+      socket.connect();
+      setInterval(() => {
+        if (
+          transcriptRef.current.trim() != "" &&
+          lastEmissionRef.current != transcriptRef.current
+        ) {
+          lastEmissionRef.current = transcriptRef.current;
+          sendSpeech(transcriptRef.current, languageRef.current, false);
+        }
+      }, speechToTextParameter.emitInterval);
+    }
   }, [router.isReady]);
 
+  useEffect(() => {
+    if (!browserSupportsSpeechRecognition) {
+      setModalText("Your browser does not support speech recognition.");
+      onOpen();
+    } else if (!isMicrophoneAvailable) {
+      setModalText("Microphone access is not permitted.");
+      onOpen();
+    }
+  }, [isMicrophoneAvailable, browserSupportsSpeechRecognition]);
+
+  function onMessageEnd(event: any) {
+    resetTranscript();
+    lastEmissionRef.current = "";
+    SpeechRecognition.getRecognition()!.lang = language;
+    SpeechRecognition.getRecognition()?.start();
+  }
+
+  function toggleListening() {
+    if (listening) {
+      SpeechRecognition.getRecognition()!.onend = () => {};
+      SpeechRecognition.getRecognition()?.abort();
+      setListenning(false);
+    } else {
+      SpeechRecognition.getRecognition()!.continuous = false;
+      SpeechRecognition.getRecognition()!.lang = language;
+      SpeechRecognition.getRecognition()?.start();
+      setListenning(true);
+      SpeechRecognition.getRecognition()!.onend = onMessageEnd;
+      SpeechRecognition.getRecognition()!.onspeechstart = () => {
+        sendSpeech("break", languageRef.current, true);
+      };
+    }
+  }
+
+  async function sendSpeech(
+    speech: string,
+    language: string,
+    isBreak: boolean
+  ) {
+    if (speech == "") {
+      return;
+    }
+    await socket.emit("speech", {
+      speech: speech,
+      language: speechToTranslate.get(language),
+      seq: sequenceRef.current,
+      isBreak: isBreak,
+    });
+    sequenceRef.current = sequenceRef.current + 1;
+  }
+
   return (
-    <Stack alignItems="center" spacing={8}>
-      <Heading size="xl" color={colorTheme.primary}>
-        {errorMessage != "" ? errorMessage : "Session #" + sessionId}
-      </Heading>
-      <Box h="30vh" w="70vw" overflowX="hidden" overflowY="scroll">
-        <Heading size="lg" color="#92989c" textAlign="center">
-          {/* {subtitleHistory.map((s,id)=>{
-                        return (<div key={id}>{s}</div>);
-                    })
-                    }  */}
-          <div>{subtitleHistory[subtitleHistory.length]}</div>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Error</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{modalText}</ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Stack alignItems="center" spacing={5}>
+        <Heading size="xl" color={colorTheme.primary}>
+          Session #{sessionId}
         </Heading>
-        <Heading size="lg" color={colorTheme.primary} textAlign="center">
-          {currentSubtitle}
-        </Heading>
-        <div ref={transcriptContainer}></div>
-      </Box>
-      <Box w="30vw">
-        <Text>Subtitle language</Text>
-        <Select
-          onChange={(e) => {
-            setLanaguage(e.target.value);
-            socket.emit("changeLanguage", {
-              language: e.target.value,
-              sessionId: sessionId,
-            });
-          }}
-          defaultValue={languageSpeechTags[0].tag}
-          bgColor="white"
-        >
-          {languageTranslateTag.map((e) => {
-            return (
-              <option value={e.tag} key={e.tag}>
-                {e.name}
-              </option>
-            );
-          })}
-        </Select>
-      </Box>
-    </Stack>
+        <Box h="30vh" w="70vw" overflowX="hidden" overflowY="scroll">
+          <Heading size="lg" color="#92989c" textAlign="center">
+            <div>{subtitleHistory[subtitleHistory.length]}</div>
+          </Heading>
+          <Heading size="lg" color={colorTheme.primary} textAlign="center">
+            {currentSubtitle}
+          </Heading>
+          <div ref={transcriptContainer}></div>
+        </Box>
+        <Box w="30vw">
+          <Text>You are a participant.</Text>
+          <Text>Speech language</Text>
+          <Select
+            onChange={(e) => {
+              setLanaguage(e.target.value);
+            }}
+            bgColor="white"
+            defaultValue={languageSpeechTags[0].tag}
+          >
+            {languageSpeechTags.map((e) => {
+              return (
+                <option value={e.tag} key={e.tag}>
+                  {e.name}
+                </option>
+              );
+            })}
+          </Select>
+          <Text>Subtitle language</Text>
+          <Select
+            onChange={(e) => {
+              setSubtitleLanguage(e.target.value);
+              socket.emit("changeLanguage", {
+                language: e.target.value,
+                sessionId: sessionId,
+              });
+            }}
+            bgColor="white"
+            defaultValue={languageTranslateTag[0].tag}
+          >
+            {languageTranslateTag.map((e) => {
+              return (
+                <option value={e.tag} key={e.tag}>
+                  {e.name}
+                </option>
+              );
+            })}
+          </Select>
+        </Box>
+        {
+          <Button
+            bgColor={colorTheme.primary}
+            color="white"
+            onClick={toggleListening}
+          >
+            {listening ? "Stop listening" : "Start listening"}
+          </Button>
+        }
+      </Stack>
+    </>
   );
 }
